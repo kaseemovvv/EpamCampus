0aedadd50f90951e2e1cefca2a8ae07d24411cf2
kaseemovvv <kasymovr1120@gmail.com>
2024-08-01 23:03:20 +0500

    User service

---
+++ src/main/java/com/epam/learning/service/UserService.java
@@ -0,0 +1,20 @@
+package com.epam.learning.service;
+
+import com.epam.learning.dto.BoardReqDto;
+import com.epam.learning.dto.BoardResDto;
+import com.epam.learning.dto.UserReqDto;
+import com.epam.learning.dto.UserResDto;
+
+import java.util.List;
+
+public interface UserService {
+    UserResDto createUser(UserReqDto board, Integer userId);
+
+    UserResDto updateUser(Integer id, UserReqDto board);
+
+    String deleteUser(Integer id);
+
+    UserResDto getUser(Integer id);
+
+    List<UserResDto> getAllUsers(Integer userId);
+}
---
+++ src/main/java/com/epam/learning/service/impl/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.epam.learning.service.impl;
+
+public class UserServiceImpl {
+}
